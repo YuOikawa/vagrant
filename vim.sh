7915e851504388962fc308459803d0aae9930cd4
YuOikawa <f.jesus4123@gmail.com>
2020-08-14 13:31:51 +0900

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,7 +1,16 @@
 #!bin/sh
 sudo yum -y install mercurial ncurses-devel make gcc git
+sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
+sudo yum install -y python35u python35u-libs python35u-devel python35u-pip
+sudo yum -y install ncurses-devel
 git clone https://github.com/vim/vim.git
-cd vim/src
-sudo make
+cd vim
+
+./configure \
+--enable-fail-if-missing \
+--enable-python3interp \
+--with-python3-config-dir=/usr/lib64/python3.5/config-3.5m \
+vi_cv_path_python3=/usr/bin/python3.5
+
 sudo make install
 cd ~
